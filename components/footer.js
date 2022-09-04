87960e02b76a46ad981bd08a0d919c34fd07d827
Tatenda Ganyani <tatendaganyani5@gmail.com>
2022-09-04 22:39:04 +0300

    add github link

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export default function Footer(){
     return (
         <div style={{display:"flex", flexFlow:"row nowrap" ,justifyContent:"space-around", minHeight:"200px", backgroundColor:"black", color:"white", paddingTop:"70px"}}>
             <div>
-                <GitHubIcon />
+                <a href='https://github.com/tganyani/kitchen-store'><GitHubIcon /></a>
             </div>
             <div>
             Tgb &copy; 2022
